6343b4ea8c7222b7ea66ece4b99426fdf0f5c95b
rimpo <manojb@care24.co.in>
2019-07-21 10:49:25 +0530

    working max server/client with TLS

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,18 +5,36 @@ import (
 	"flag"
 	"io"
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/rimpo/maxx/pkg/max"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
 	serverAddr = flag.String("server_addr", "127.0.0.1:8080", "The server address in the format of host:port")
+	count      = flag.Int("count", 10, "count of random numbers. (default: 10)")
+	certFile   = flag.String("cert_file", "", "server certificate file for TLS")
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 
-	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
+	flag.Parse()
+
+	if *certFile == "" {
+		log.Fatal("TLS certificate not supplied.")
+	}
+
+	// Create the client TLS credentials
+	creds, err := credentials.NewClientTLSFromFile(*certFile, "")
+	if err != nil {
+		log.Fatalf("could not load tls cert: %s", err)
+	}
+
+	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("failed to connect. %v", err)
 	}
@@ -50,8 +68,9 @@ func main() {
 	}()
 
 	//send on stream
-	for i := 0; i < 10; i++ {
-		if err := stream.Send(&max.Request{Num: int32(i)}); err != nil {
+	for i := 0; i < *count; i++ {
+		//send random number from 1 to 100
+		if err := stream.Send(&max.Request{Num: int32(rand.Intn(100) + 1)}); err != nil {
 			log.Fatalf("Failed to send a note: %v", err)
 		}
 	}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,10 +9,13 @@ import (
 
 	"github.com/rimpo/maxx/pkg/max"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
-	port = flag.Int("port", 8080, "server listen port e.g: 8080. (default:8080)")
+	port     = flag.Int("port", 8080, "server listen port e.g: 8080. (default:8080)")
+	certFile = flag.String("cert_file", "", "server certificate file path.")
+	keyFile  = flag.String("key_file", "", "server private key file path.")
 )
 
 type maxServer struct {
@@ -20,33 +23,53 @@ type maxServer struct {
 
 //max number will be returned in stream
 func (m maxServer) FindMaxNumber(stream max.Service_FindMaxNumberServer) error {
+	var maxNumber int32 = -1
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
+			log.Printf("stream closed. %v", err)
 			return nil
 		}
 		if err != nil {
+			log.Printf("stream error. %v", err)
 			return err
 		}
 
 		log.Printf("received:%v", in.Num)
-
-		if err := stream.Send(&max.Response{Max: in.Num}); err != nil {
+		if maxNumber < in.Num {
+			maxNumber = in.Num
+		}
+		if err := stream.Send(&max.Response{Max: maxNumber}); err != nil {
 			return err
 		}
 	}
 }
 
 func main() {
+	flag.Parse()
 
 	log.Printf("listen on port %v", *port)
 
+	//List on port
 	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen on port:%d", *port)
 	}
 
-	server := grpc.NewServer()
+	if *certFile == "" || *keyFile == "" {
+		log.Fatal("TLS keys not supplied.")
+	}
+
+	// Create the TLS credentials
+	creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+	if err != nil {
+		log.Fatalf("could not load TLS keys: %s", err)
+	}
+	// Create an array of gRPC options with the credentials
+	opts := []grpc.ServerOption{grpc.Creds(creds)}
+
+	// create a gRPC server object
+	server := grpc.NewServer(opts...)
 
 	//register handler
 	max.RegisterServiceServer(server, &maxServer{})
